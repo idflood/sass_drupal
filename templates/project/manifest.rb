974eb103707f633a4c0ac3b2568e7d500fa8b448
David Mignot <david.mig@gmail.com>
2011-04-14 16:56:34 +0200

    add missing image, set bg with image_url

--- templates/project/manifest.rb
+++ templates/project/manifest.rb
@@ -1,4 +1,6 @@
 # Make sure you list all the project template files here in the manifest.
 stylesheet '_sass_drupal.sass', :media => 'screen, projection'
 
+discover :images
+
 description "sass version of drupal base/system css."
